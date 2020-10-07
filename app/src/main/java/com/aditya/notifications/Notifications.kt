3557846ce60f9451a956ed2fead5f82a8c530415
Aditya <icgadi99@gmail.com>
2020-10-07 09:42:10 +0530

    Created Application class for notif channels

---
+++ app/src/main/java/com/aditya/notifications/Notifications.kt
@@ -0,0 +1,39 @@
+package com.aditya.notifications
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.core.content.getSystemService
+
+class Notifications : Application() {
+    /* Assigning ids to different notification channels */
+    private val CHANNEL_ONE: String  = "channel1"
+    private val CHANNEL_TWO: String = "channel2"
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannels()
+    }
+
+    private fun createNotificationChannels() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel1 = NotificationChannel(
+                CHANNEL_ONE,
+                "Channel 1",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel1.description = "This is Notification Channel #1"
+
+            val channel2 = NotificationChannel(
+                CHANNEL_TWO,
+                "Channel 2",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            channel2.description = "This is Notification Channel #2"
+
+            val notificationManager: NotificationManager = getSystemService(NotificationManager::class.java)!!
+            notificationManager.createNotificationChannel(channel1)
+            notificationManager.createNotificationChannel(channel2)
+        }
+    }
+}
